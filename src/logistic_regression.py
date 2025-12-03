a3e87404c2ae4bca7e4715f5ea16ab959b0c2248
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-04 03:26:38 +0700

    chore: setup logistic regression for multiclass predictions

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,36 +1,51 @@
 import numpy as np
+import numpy.typing as npt
 import pickle
+from typing import Literal, Optional
+
+FloatArray = npt.NDArray[np.float64]
+IntArray = npt.NDArray[np.int_]
 
 class LogisticRegressionModel:
-    def __init__(self, learning_rate=0.01, n_iters=1000):
-        self.lr = learning_rate
-        self.n_iters = n_iters
-        self.weights = None
+    def __init__(self, learning_rate: float = 0.01, n_iters: int = 1000, multi_class: Literal['ovr', 'multinomial'] = 'ovr'):
+        self.lr: float = learning_rate
+        self.epochs: int = n_iters
+        self.multi_class: Literal['ovr', 'multinomial'] = multi_class
+        self.weights: Optional[FloatArray] = None
         self.bias = None
-        self.losses = [] # Untuk bonus video
-        self.history_params = [] # Untuk bonus video
+        self.classes = None
+        self.losses = []
+        self.history_params = []
 
-    def _sigmoid(self, x):
+    def _sigmoid(self, x: FloatArray) -> FloatArray:
         return 1 / (1 + np.exp(-x))
+    
+    def _softmax(self, x: FloatArray) -> FloatArray:
+        # TODO: Implementasi fungsi softmax
+        pass
 
-    def fit(self, X, y):
+    def fit(self, X: FloatArray, y: FloatArray):
         n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+        self.classes = np.unique(y)
+        n_classes = len(self.classes)
+        self.weights = np.zeros((n_features, n_classes))
+        self.bias = np.zeros(n_classes)
 
-        # Gradient Descent
-        for _ in range(self.n_iters):
-            # TODO: Implementasi forward pass (linear model + sigmoid)
-            
-            # TODO: Hitung Gradient dw dan db
-            
-            # TODO: Update weights dan bias
-            
-            # Simpan history untuk bonus visualisasi
-            pass
+        if self.multi_class == 'ovr':
+            self._fit_ovr(X, y, n_samples, n_features, n_classes)
+        else:
+            self._fit_softmax(X, y, n_samples, n_features, n_classes)
+    
+    def _fit_ovr(self, X: FloatArray, y: FloatArray, n_samples: int, n_features: int, n_classes: int) -> None:
+        # TODO: Implementasi One-vs-Rest LogisticRegression Model
+        pass
 
-    def predict(self, X):
-        # TODO: Implementasi prediksi kelas (0 atau 1) berdasarkan threshold 0.5
+    def _fit_softmax(self, X: FloatArray, y: FloatArray, n_samples: int, n_features: int, n_classes: int) -> None:
+        # TODO: Implementasi Softmax Regression
+        pass
+
+    def predict(self, X: FloatArray):
+        # TODO: Implementasi prediksi multikelas
         pass
 
     def save_model(self, filename):
